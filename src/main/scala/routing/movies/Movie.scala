bac59f178e157c3b0f09aa2ac760a3664469451f
dinaraparanid <dinaraparanid@gmail.com>
2026-06-14 13:36:23 +0300

    movie endpoint response fix

--- src/main/scala/routing/movies/Movie.scala
+++ src/main/scala/routing/movies/Movie.scala
@@ -3,13 +3,19 @@ package routing.movies
 
 import cats.effect.IO
 
+import com.paranid5.twin_peaks_tv_server.routing.movies.entity.Movie
 import com.paranid5.twin_peaks_tv_server.routing.movies.response.movie
 
 import sttp.tapir.*
+import sttp.tapir.json.circe.jsonBody
+import sttp.tapir.generic.auto.*
 
-private def movieEndpoint =
+private def movieEndpoint = {
+  val data = movie
+
   endpoint.get
     .in("movie")
-    .out(stringJsonBody)
+    .out(jsonBody[Movie] example data)
     .serverLogic[IO]: _ =>
-      IO pure Right(movie.toString)
+      IO pure Right(movie)
+}
